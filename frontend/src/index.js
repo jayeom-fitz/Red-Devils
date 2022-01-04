ed90d02ee58dc1e1bd1fe3cc614bc38f8ea348c4
jayEom <djawjdgus93@naver.com>
2022-01-04 18:27:31 +0900

    #F-0-3 : redux init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,12 @@ import ReactDOM from 'react-dom';
 
 // third party
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 // project imports
 import * as serviceWorker from 'serviceWorker';
 import App from 'App';
+import { store } from 'store';
 
 // style + assets
 import 'assets/scss/style.scss';
@@ -13,9 +15,11 @@ import 'assets/scss/style.scss';
 // ==============================|| REACT DOM RENDER  ||============================== //
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
 
