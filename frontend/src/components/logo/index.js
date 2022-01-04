9d066a79c2196626482b3fb68663172563f8a4fa
jayEom <djawjdgus93@naver.com>
2022-01-04 18:19:30 +0900

    #F-0-2 : header - logo

---
+++ frontend/src/components/logo/index.js
@@ -0,0 +1,10 @@
+// logo image
+import logo from 'assets/images/logo.png';
+
+// ==============================|| LOGO PNG ||============================== //
+
+const Logo = () => {
+  return <img src={logo} alt="Red Devils" width="160" />;
+};
+
+export default Logo;
--- frontend/src/views/layout/main/index.js
+++ frontend/src/views/layout/main/index.js
@@ -1,7 +1,24 @@
+import { Outlet } from 'react-router-dom';
+
+// material-ui
+import { styled, useTheme } from '@mui/material/styles';
+import { AppBar, Box, CssBaseline, Toolbar, useMediaQuery } from '@mui/material';
+
+// project imports
+import Header from './header';
+
+// assets
+import { IconChevronRight } from '@tabler/icons';
+
 // ==============================|| MAIN LAYOUT ||============================== //
 
 const MainLayout = () => {
-  return <>main layout</>;
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Header />
+      main
+    </Box>
+  );
 };
 
 export default MainLayout;
