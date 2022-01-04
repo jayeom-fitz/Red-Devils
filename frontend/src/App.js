0bfb00cf7d1df5bddaf140a5c01e039aaebb8f24
jayEom <djawjdgus93@naver.com>
2022-01-04 18:51:12 +0900

    #F-0-4 : app init with theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,33 @@
+import { useSelector } from 'react-redux';
+
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
+
 // routing
 import Routes from 'routes';
+
+// defaultTheme
+import themes from 'views/themes';
+
+// project imports
+import NavigationScroll from 'views/layout/navigation-scroll';
 
 // ==============================|| APP ||============================== //
 
 const App = () => {
+  const customization = useSelector((state) => state.customization);
+
   return (
     <>
-      <Routes />
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={themes(customization)}>
+          <CssBaseline />
+
+          <NavigationScroll>
+            <Routes />
+          </NavigationScroll>
+        </ThemeProvider>
+      </StyledEngineProvider>
     </>
   );
 };
