a723b5f0e789df3cd68815247447386242a55934
jayEom <djawjdgus93@naver.com>
2022-01-07 18:10:47 +0900

    #F-0-8 : new routes and mini layout

---
+++ frontend/src/routes/AuthRoutes.js
@@ -0,0 +1,20 @@
+// import { lazy } from 'react';
+
+// project imports
+// import Loadable from 'components/Loadable';
+import MinimalLayout from 'views/layout/mini';
+
+// ==============================|| AUTHENTICATION ROUTING ||============================== //
+
+const AuthenticationRoutes = {
+  path: '/',
+  element: <MinimalLayout />,
+  // children: [
+  //   {
+  //     path: '/login',
+  //     element: <AuthLogin />,
+  //   },
+  // ],
+};
+
+export default AuthenticationRoutes;
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,9 +2,10 @@ import { useRoutes } from 'react-router-dom';
 
 // routes
 import MainRoutes from './MainRoutes';
+import AuthRoutes from './AuthRoutes';
 
 // ==============================|| ROUTING RENDER ||============================== //
 
 export default function Routes() {
-  return useRoutes([MainRoutes]);
+  return useRoutes([MainRoutes, AuthRoutes]);
 }
