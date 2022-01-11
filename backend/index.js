135836f3c55371580853c097a7663e935b7a44d6
jayEom <djawjdgus93@naver.com>
2022-01-11 17:30:55 +0900

    #B-0-2 : index

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+
+const server = express();
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(cors());
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
